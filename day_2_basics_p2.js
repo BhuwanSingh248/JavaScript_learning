8bf17c50c26f98fbcd1cd498fe9b225b46d9ca10
root <bhuwansingh248@gmail.com>
2021-10-27 01:16:59 +0530

    basics of function

---
+++ day_2_basics_p2.js
@@ -0,0 +1,27 @@
+// #### introduction day 2
+
+// function 
+
+function plus_1(x){
+    return x+1;
+}
+var y = 10
+console.log(plus_1(y))
+console.log(y)
+
+// when we combine functions with objects we get method 
+
+var points = [
+    {x:0, y:2},
+    {x:2, y:4}
+];
+
+points.dist = function() {
+    var p1 = this[0];
+    var p2 = this[1];
+    var a = p2.x - p1.x 
+    var b = p2.y - p1.y
+    return Math.sqrt(a*a + b*b)  
+}
+
+console.log(points.dist())
